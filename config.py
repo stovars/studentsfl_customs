d4e4d95500772a804e561cf4aad9fd70033d2f68
stovars <nikitastaroselskij9@gmail.com>
2026-04-09 19:39:07 +0300

    first commit

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///university.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "my-secret-key-123"
+    
     ALL_PERMISSIONS = {
         "students_page": ["create", "read", "update", "delete"],
          "teachers_page": ["create", "read", "update", "delete"]
